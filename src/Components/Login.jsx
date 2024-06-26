0902e8859d76d660423c13ac4b8845f9b3cb2a87
Duy Le <aaron.le1995@gmail.com>
2024-06-26 17:14:20 +0700

    Login page v 0.0

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,7 +1,102 @@
 import React from "react";
+import "./login.css";
+import { SpotifyFilled } from "@ant-design/icons";
 
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Checkbox, Form, Input, ConfigProvider } from "antd";
 const Login = () => {
-  return <div>Login</div>;
+  const onFinish = (values) => {
+    console.log("Success:", values);
+    localStorage.setItem("userData", JSON.stringify(values));
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <>
+      <div className="login-div">
+        <SpotifyFilled style={{ fontSize: 60 }} />
+        <h1>Log in to Music Apps </h1>
+        <Form
+          name="normal-login"
+          variant="filled"
+          className="login-form"
+          layout="vertical"
+          // labelCol={{
+          //   span: 8,
+          // }}
+          wrapperCol={{
+            span: 24,
+          }}
+          style={{
+            maxWidth: 700,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password
+              prefix={<LockOutlined className="site-form-item-icon" />}
+            />
+          </Form.Item>
+
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{
+              // offset: 0,
+              span: 6,
+            }}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item
+          // wrapperCol={{
+          //   offset: 8,
+          //   span: 16,
+          // }}
+          >
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+              style={{ fontSize: 22 }}
+            >
+              Log In
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </>
+  );
 };
 
 export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Route } from "react-router-dom";
+import Login from "./Components/Login.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Login />
   </React.StrictMode>
 );
